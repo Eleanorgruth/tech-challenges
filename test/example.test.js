fc03556d5577a3e1e24cff25fa7561a3d853b036
Eleanor Ruth <ellie.ruth@me.com>
2023-03-01 10:31:35 -0700

    add tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,11 +1,26 @@
-// import chai from "chai"
-// const expect = chai.expect
+
 const { expect } = require('chai');
 const findNextPalindrome = require("../techChallenge2"); 
 
 describe('findNextPalindrome', (input) => {
   it('should return the correct output', () => {
+    input = 100
     const result = findNextPalindrome(input);
-    expect(result).to.equal(input);
+    expect(result).to.equal(101);
+  });
+  it('should return the correct output', () => {
+    input = 101
+    const result = findNextPalindrome(input);
+    expect(result).to.equal(111);
+  });
+  it('should return the correct output', () => {
+    input = 111
+    const result = findNextPalindrome(input);
+    expect(result).to.equal(121);
+  });
+  it('should return the correct output', () => {
+    input = 90
+    const result = findNextPalindrome(input);
+    expect(result).to.equal(99);
   });
 });
