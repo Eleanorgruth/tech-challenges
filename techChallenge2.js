16901db4d5f28cd7f517213200683a1909d5f398
Eleanor Ruth <ellie.ruth@me.com>
2023-03-01 10:33:25 -0700

    solve palindrome problem

--- techChallenge2.js
+++ techChallenge2.js
@@ -4,10 +4,22 @@
 
 // findNextPalindrome(101)
 // => 111
+//I want to take number + 1
+//Check if it is a paldrone, if it is return it. if not plus one again until it is
 
-const findNextPalindrome = ((number) => {
-  
-  return number
+const isPalindrome = ((number) =>  {
+  const stringNumber = String(number)
+  const reversedStringNum = stringNumber.split("").reverse().join("")
+  return stringNumber === reversedStringNum
 })
 
+const findNextPalindrome = ((number)=> {
+  const newNumber = number + 1
+  if (isPalindrome(newNumber)) {
+    return newNumber
+  }
+  return findNextPalindrome(newNumber);
+})
+
+console.log(findNextPalindrome(111))
 module.exports = findNextPalindrome
