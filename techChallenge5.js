0228114de32c3d950c70073b92b451e371241c3c
Eleanor Ruth <ellie.ruth@me.com>
2023-03-03 10:15:27 -0700

    Create Subsets tech challenge

---
+++ techChallenge5.js
@@ -0,0 +1,20 @@
+// #Example 1
+// Input: [1, 2, 3, 4]
+// Output: [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
+
+// #Example 2
+// Input: [54, 77]
+// Output[[54, 77]]
+
+// #Example 3
+// Input: []
+// Output: []
+
+const createSubsets = ((array) => { 
+  const newArray = []
+  array.forEach(element => {
+    return [element, ]
+  });
+  // if(!array[0])
+})
+module.exports = createSubsets
